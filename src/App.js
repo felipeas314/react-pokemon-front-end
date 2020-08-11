426172a8b58b8bb3bd0bd2f33608658a4df25a6d
Felipe <felipeas314@gmail.com>
2020-08-10 21:58:07 -0300

    criação das páginas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
 import Header from './components/header';
+import { Router, Route, Switch } from 'react-router-dom';
+
+import history from './service/history';
+
+import Main from './pages/main';
+import About from './pages/about';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <GlobalStyle />
       <Header />
-      Hello World
+      <Router history={history}>
+        <Switch>
+          <Route path='/' exact component={Main}/>
+          <Route path='/about'  component={About}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
